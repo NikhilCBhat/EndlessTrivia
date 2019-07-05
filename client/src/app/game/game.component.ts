6b91984d26958a38cf455b403a95c8227b73f2b3
Zirui Hao <zirui.hao.22@dartmouth.edu>
2019-07-05 15:48:58 -0700

    built reformatting function to edit the structure of the questions for the front-end app

--- client/src/app/game/game.component.ts
+++ client/src/app/game/game.component.ts
@@ -8,17 +8,49 @@ import {ContentService} from '../content/content.service';
     styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-    currentIndex;
+    currentQuestion;
+    currentQuestionIndex;
     finished;
     constructor(
         private route: ActivatedRoute,
         private cartService: ContentService
       ) {
-          this.currentIndex = 0;
+          this.currentQuestionIndex = 0;
           this.finished = false;
        }
     ngOnInit() {}
 
-    nextQuestion() { this.currentIndex = this.cartService.switchQuestion(true); }
-    prevQuestion() {this.currentIndex = this.cartService.switchQuestion(false); }
+    nextQuestion() { this.currentQuestionIndex = this.cartService.switchQuestion(true); }
+    prevQuestion() {this.currentQuestionIndex = this.cartService.switchQuestion(false); }
+
+    reformatQuestion(question) {
+        this.currentQuestion = {
+            prompt: question.question,
+            allAnswers: [],
+            correctAnswer: question.correctAnswer,
+            correctAnswerIndex: Math.floor(Math.random() * (question.wrongAnswers.length + 1)),
+            selectionIndex: -1,
+        };
+        question.wrongAnswers.forEach(answer => {
+            this.currentQuestion.allAnswers.append({
+                text: answer,
+                state: 'unselected'
+            });
+        });
+        this.currentQuestion.allAnswers = question.wrongAnswers.splice(this.currentQuestion.correctAnswerIndex, 0, {
+            text: this.currentQuestion.correctAnswer,
+            state: 'unselected'
+        });
+    }
+    selectAnswer(index) {
+        this.currentQuestion.selectionIndex = index;
+        if (this.currentQuestion.allAnswers[index].text !== this.currentQuestion.correctAnswer) {
+            this.currentQuestion.allAnswers[index].state = 'incorrect';
+        }
+        this.currentQuestion.allAnswers[this.currentQuestion.correctAnswerIndex].state = 'correct';
+    }
+
+    saveSelection() {
+        this.cartService.updateQuestionWithUserInput(this.currentQuestion, this.currentQuestionIndex);
+    }
   }
